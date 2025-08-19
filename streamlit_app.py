524da4ab245c650d680e0ee4c6f4788cef78158d
henishshah18 <henishshah18@gmail.com>
2025-08-19 18:24:32 +0530

    fix4

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,16 +3,31 @@ from rag import graph
 from langchain_chroma import Chroma
 
 
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+
+# Display chat messages from history on app rerun
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+
 with st.chat_message("assistant"):
     st.write("Hi, what exercise would you like to know about?")
 # message.write("Hi, what exercise would you like to know about?")
+
 prompt = st.chat_input("Enter query here")
 if prompt:
+    # Display user message in chat message container
     with st.chat_message("user"):
-        st.write(f"{prompt}")
-    
-    message = st.chat_message("assistant")
-    response  = graph.invoke({'question':prompt})
-    message.write(response["answer"])
+        st.markdown(prompt)
+    # Add user message to chat history
+    st.session_state.messages.append({"role": "user", "content": prompt})
+
+
+response  = graph.invoke({'question':prompt})
+with st.chat_message("assistant"):
+    st.markdown(response['answer'])
+# Add assistant response to chat history
+st.session_state.messages.append({"role": "assistant", "content": response['answer']})
     
 
