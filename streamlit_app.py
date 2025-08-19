fe8d5361679493432f74efb7776af6837ae616b2
henishshah18 <henishshah18@gmail.com>
2025-08-19 18:13:04 +0530

    fix3

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,13 +6,10 @@ from langchain_chroma import Chroma
 with st.chat_message("assistant"):
     st.write("Hi, what exercise would you like to know about?")
 # message.write("Hi, what exercise would you like to know about?")
-while True:
-    prompt = st.chat_input("Enter query here")
-    if prompt=='Bye':
-        break
-    if prompt:
-        with st.chat_message("user"):
-            st.write(f"{prompt}")
+prompt = st.chat_input("Enter query here")
+if prompt:
+    with st.chat_message("user"):
+        st.write(f"{prompt}")
     
     message = st.chat_message("assistant")
     response  = graph.invoke({'question':prompt})
